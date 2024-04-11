f99ac28cf8106b7d581526d3b0ba78b350f0028d
ABDALRAHMAN <abdalrahmandaas963@gmail.com.com>
2024-04-11 22:04:56 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import Aboutme from "./components/Aboutme";
 import Landingpage from "./components/Landingpage";
-import useScreenSize from "../utils/useScreenSize";
+
 
 
 function App() {
-  const { isSmall, isMedium, isLarge } = useScreenSize();
+  
 
   return (
     <div className="App">
